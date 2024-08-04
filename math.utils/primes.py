c44d64fc364dbd457f0ccaea8c786719d9d7f4e6
RyanLim4 <rrl23@ic.ac.uk>
2024-08-04 12:02:54 +0800

    math.utils added

---
+++ math.utils/primes.py
@@ -0,0 +1,10 @@
+def isprime(n):
+    from math import sqrt
+    
+    if n <=1:
+        return False
+    
+    for i in range(2, int(sqrt(n))+1):
+        if n % i == 0:
+            return False
+    return True
